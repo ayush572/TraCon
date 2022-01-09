9c282fc7537943ee18772e315b979dceeda6d9d7
ayush572 <ayushdagaeinstein@gmail.com>
2022-01-09 09:33:21 +0530

    planlist each class toggle working

--- src/components/plan_levels.js
+++ src/components/plan_levels.js
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import '../styles/planlvl.scss'
+
 const Levels = (props) =>{
+    const [de,setDe]=useState([0])
     const lvls=props.no;
     return (
         <div className="levels_data">
             {lvls.map((lvl)=>(
                 <div className='exlist'>
-                    <div className="list">
+                    <div className="list" onClick={()=>{console.log(de);
+                    if(de === lvl.id){
+                        setDe(0);
+                    }
+                    else{
+                        setDe(lvl.id);
+                    }
+                    console.log(de);
+                    }}>
                         <h1>{lvl.title}</h1>
                     </div>
-                    <div className="list">
-                        <h3>{lvl.exercise1}</h3>
+                    <div className={`list${lvl.id} ${de === lvl.id && "visible"}`}>
+                        <div className="list0">
+                            <h3>{lvl.exercise1}</h3>
+                        </div>
+                        <div className="list0">
+                            <h3>{lvl.exercise2}</h3>
+                        </div>
+                        
+                        <div className="list0">
+                            <h3>{lvl.exercise3}</h3>
+                        </div>
                     </div>
-                    <div className="list">
-                        <h3>{lvl.exercise2}</h3>
-                    </div>
-                    
-                    <div className="list">
-                        <h3>{lvl.exercise3}</h3>
-                    </div>
-                    
                 </div>
             ))}
         </div>
--- src/components/planlvl.js
+++ src/components/planlvl.js
@@ -3,15 +3,15 @@ import Levels from './plan_levels';
 
 const PlanLevel=()=> {
     const [lvls,setLvls]=useState([
-        {title: 'Plan Level for Underweight', exercise1: '10 Dumbell', exercise2: '12 Squats', exercise3: '10 leg raise'},
-        {title: 'Plan Level for Healthy Weight', exercise1: '15 Dumbell', exercise2: '15 Squats', exercise3: '12 leg raise'},
-        {title: 'Plan Level for Overweight', exercise1: '5 Dumbell', exercise2: '12 Squats', exercise3: '15 leg raise'},
-        {title: 'Plan Level for Obese', exercise1: '0 Dumbell', exercise2: '20 Squats', exercise3: '15 leg raise'},
+        {title: 'Plan Level for Underweight', exercise1: '10 Dumbell', exercise2: '12 Squats', exercise3: '10 leg raise', id: 1},
+        {title: 'Plan Level for Healthy Weight', exercise1: '15 Dumbell', exercise2: '15 Squats', exercise3: '12 leg raise', id: 2},
+        {title: 'Plan Level for Overweight', exercise1: '5 Dumbell', exercise2: '12 Squats', exercise3: '15 leg raise', id: 3},
+        {title: 'Plan Level for Obese', exercise1: '0 Dumbell', exercise2: '20 Squats', exercise3: '15 leg raise', id: 4},
     ])
     return (
         <div className="level">
             <div className='levellist'>
-                <Levels no={lvls} /> 
+                <Levels no={lvls}/> 
             </div>
         </div>
     )
